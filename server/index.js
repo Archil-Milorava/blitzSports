2495b6b0d7bad38df983ebd336a8e0c4b5bb7a36
Archil-Milorava <achimilorava16@gmail.com>
2025-08-09 20:58:01 +0400

    fix: post article

--- server/index.js
+++ server/index.js
@@ -23,8 +23,7 @@ app.use(
 );
 
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/auth", googleAuthRoute);
 app.use("/api/v1/user", userRoutes);
