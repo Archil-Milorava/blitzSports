5fb31d7acec33f8109d9bea072cc84d6918043d4
Archil Milorava <archilmilorava@Archils-MacBook-Pro.local>
2025-10-02 21:26:10 +0400

    testin ci/cd 7

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/article", articleRoute);
 
 app.use("/api/test/ci", (req, res) => {
-  res.send("test of ci/cd 2")
+  res.send("test of ci/cd 4")
 })
 
 app.use((req, res) => {
